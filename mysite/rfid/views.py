a9d1f8866d2c25724f4e8a171339091289d99cb4
ayushkul2910 <kulshrestha.ayush2910@gmail.com>
2018-04-01 01:48:26 +0530

    new css

--- mysite/rfid/views.py
+++ mysite/rfid/views.py
@@ -22,47 +22,50 @@ def Ard(I):
 
 def index(request):
     template=loader.get_template('index1.html')
-    J_ID=1 #different for every bus
-    l=User_Journey.objects.all()
-    C=Cost.objects.all()
-    Pass=Passenger.objects.all()
-    price=0
-    User_name=''
-    for i in C:
-        if(i.J_ID==str(J_ID)):
-            price=i.Price
-    S="Access Denied"
-    for i in range(len(l)):
-        record=l[i]
-        I=record.id
-        U=record.User_id
-        J=record.Journey_id
-        if int(J)==J_ID:
-            flag,serin=Ard(U)
-            if(flag):
-                for h in Pass:
-                    if(h.User_id==serin):
-                        User_name=h.User_name
-                        h.Credit=str(int(h.Credit)-int(price))
-                        if(int(h.Credit)<0):
-                            flag=False
-                            S="Insufficient balance. Kindly Recharge to proceed."
-                        else:
-                            h.save(update_fields=["Credit"])
+    try:
+        J_ID=1 #different for every bus
+        l=User_Journey.objects.all()
+        C=Cost.objects.all()
+        Pass=Passenger.objects.all()
+        price=0
+        User_name=''
+        for i in C:
+            if(i.J_ID==str(J_ID)):
+                price=i.Price
+        S="Access Denied"
+        for i in range(len(l)):
+            record=l[i]
+            I=record.id
+            U=record.User_id
+            J=record.Journey_id
+            if int(J)==J_ID:
+                flag,serin=Ard(U)
                 if(flag):
-                    S="Access Granted"
-                    record.Session_id=True
-                    record.save(update_fields=["Session_id"])
-                    break
-                
-    context={
-        'S':S,
-        'U':User_name,
-    }
+                    for h in Pass:
+                        if(h.User_id==serin):
+                            User_name=h.User_name
+                            h.Credit=str(int(h.Credit)-int(price))
+                            if(int(h.Credit)<0):
+                                flag=False
+                                S="Insufficient balance. Kindly Recharge to proceed."
+                            else:
+                                h.save(update_fields=["Credit"])
+                    if(flag):
+                        S="Access Granted"
+                        record.Session_id=True
+                        record.save(update_fields=["Session_id"])
+                        break
+                    
+        context={
+            'S':S,
+            'U':User_name,
+        }
 
 
-    return HttpResponse(template.render(context,request))
+        return HttpResponse(template.render(context,request))
 
+    except:
+        return HttpResponse("<h1>No RFID Reader Connected</h1>")
 
 
 
