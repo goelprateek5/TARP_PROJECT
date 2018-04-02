db13ec9d7301173f03e22a6c417fb367b872e16f
Prateek Goel <goelprateek5@gmail.com>
2018-04-03 02:11:29 +0530

    completed location backend

--- mysite/location/views.py
+++ mysite/location/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
+from rfid.models import Cost
 
 def index(request):
-	return render(request,'loc.html')
+	c=Cost.objects.all().filter(J_ID='1')
+	t=''
+	for i in c:
+		t=i.To_city
+	template = loader.get_template('loc.html')
+	context={
+	'c':t,
+	}
+	return HttpResponse(template.render(context,request))
+	# return render(request,'loc.html')
