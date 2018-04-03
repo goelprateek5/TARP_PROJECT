18e593b93a474428d5087129b2aed18512b9211f
Prateek Goel <goelprateek5@gmail.com>
2018-04-04 01:51:58 +0530

    Location Donegit status!

--- mysite/location/views.py
+++ mysite/location/views.py
@@ -7,10 +7,12 @@ def index(request):
 	c=Cost.objects.all().filter(J_ID='1')
 	t=''
 	for i in c:
+		f=i.From_city
 		t=i.To_city
 	template = loader.get_template('loc.html')
 	context={
-	'c':t,
+	't':t,
+	'f':f,
 	}
 	return HttpResponse(template.render(context,request))
 	# return render(request,'loc.html')
